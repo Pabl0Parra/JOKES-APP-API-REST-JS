fb0d3714cbe244ab05514939895a22e6f0e9aa3f
Pabl0Parra <pabsblue16@gmail.com>
2022-05-23 17:50:00 +0200

    missing actual joke ex_3

--- main.js
+++ main.js
@@ -1,38 +1,68 @@
-const API_URL = 'https://icanhazdadjoke.com/'
+const API_URL = "https://icanhazdadjoke.com/";
 
-const HTMLResponse = document.querySelector("#jokesOutput")
+const HTMLResponse = document.querySelector("#jokesOutput");
 
-async function getJokes(){
-    const response =  await fetch (API_URL, {
-        headers: {
-            'Accept': 'application/json'
-        }        
-    });
-    const data = await response.json();
+async function getJokes() {
+  const response = await fetch(API_URL, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const data = await response.json();
 
-    // Exercise 1
-    console.log(data); 
-    // Exercise 2
-    HTMLResponse.innerHTML = data.joke; 
+  // Exercise 1
+  console.log(`Ex_1_joke:`, data);
+  // Exercise 2
+  HTMLResponse.innerHTML = data.joke;
 }
-//getJokes();
-
-// fetch(`${API_URL}`).then(response => response.json).then(json => console.log(json))
+// getJokes();
 
 // Exercise 3
-const reportJokes = []
+const reportJokes = [];
+let scoreUser = 0;
+let joke = getJokes();
 
+let setScore = (value) => {
+  const currentDate = new Date();
+  let currentDateISO = currentDate.toISOString();
 
-function scoreJokes() {
-    let currentTime = new Date();
-    // Save the date in ISO format
-    let currentTimeISO = currentTime.toISOString();
-    let currentScore = {
-      date: currentTimeISO,
-      score: score,
-      joke: jokes.joke,
-    };
-    reportJokes.push(currentScore);
-    console.log("reportJokes: ", reportJokes);
+  if (value === 1) {
+    scoreUser = 1;
   }
-  scoreJokes(score)
+  if (value === 2) {
+    scoreUser = 2;
+  }
+  if (value === 3) {
+    scoreUser = 3;
+  }
+  // Need to figure a way to insert current joke into objects array
+  reportJokes.push({ joke: joke.joke, date: currentDateISO, score: scoreUser });
+  console.log("Ex_3: array with scores: ", reportJokes);
+  return scoreUser;
+};
+// function setScore(score) {
+//   let currentTime = new Date();
+//   // Save the date in ISO format
+//   let currentTimeISO = currentTime.toISOString();
+//   let currentScore = {
+//     joke: joke.joke,
+//     score: score,
+//     date: currentTimeISO,
+//   };
+//   reportJokes.push(currentScore);
+//   console.log("reportJokes: ", reportJokes);
+// }
+// //setScore(score);
+
+//Exercise 4
+const weatherResponse = document.querySelector("#weather");
+const weatherAPIKey = f2993b367d3d44028f4152530222305;
+
+async function fetchWeatherJSON() {
+  const response = await fetch(
+    "http://api.weatherapi.com/v1/current.json?key=f2993b367d3d44028f4152530222305 &q=08930&aqi=no" +
+      weatherAPIKey
+  );
+  const weather = await response.json();
+  return weather;
+}
