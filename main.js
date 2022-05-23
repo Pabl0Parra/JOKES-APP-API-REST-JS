9daedf20146d89d559e1a0825480a51f1b6829aa
Pabl0Parra <pabsblue16@gmail.com>
2022-05-23 22:41:33 +0200

    started ex_4

--- main.js
+++ main.js
@@ -19,46 +19,37 @@ async function getJokes() {
 
 // Exercise 3
 const reportJokes = [];
-let scoreUser = 0;
-let joke = getJokes();
+let userRating = 0;
+let joke = "";
 
-let setScore = (value) => {
+function setScore(value) {
   const currentDate = new Date();
   let currentDateISO = currentDate.toISOString();
 
   if (value === 1) {
-    scoreUser = 1;
+    userRating = 1;
   }
   if (value === 2) {
-    scoreUser = 2;
+    userRating = 2;
   }
   if (value === 3) {
-    scoreUser = 3;
+    userRating = 3;
   }
   // Need to figure a way to insert current joke into objects array
-  reportJokes.push({ joke: joke.joke, date: currentDateISO, score: scoreUser });
+  reportJokes.push({
+    joke: joke.joke,
+    date: currentDateISO,
+    score: userRating,
+  });
   console.log("Ex_3: array with scores: ", reportJokes);
-  return scoreUser;
-};
-// function setScore(score) {
-//   let currentTime = new Date();
-//   // Save the date in ISO format
-//   let currentTimeISO = currentTime.toISOString();
-//   let currentScore = {
-//     joke: joke.joke,
-//     score: score,
-//     date: currentTimeISO,
-//   };
-//   reportJokes.push(currentScore);
-//   console.log("reportJokes: ", reportJokes);
-// }
-// //setScore(score);
+  return userRating;
+}
 
 //Exercise 4
 const weatherResponse = document.querySelector("#weather");
-const weatherAPIKey = f2993b367d3d44028f4152530222305;
+const weatherAPIKey = "f2993b367d3d44028f4152530222305";
 
-async function fetchWeatherJSON() {
+async function getLocalWeather() {
   const response = await fetch(
     "http://api.weatherapi.com/v1/current.json?key=f2993b367d3d44028f4152530222305 &q=08930&aqi=no" +
       weatherAPIKey
